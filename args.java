65a84baba3396fb7583812faefa0d799dfc86d2f
Penny <12pover@gmail.com>
2014-11-11 15:39:38 -0500

    it works now

--- args.java
+++ args.java
@@ -1,13 +1,16 @@
-public static void main (String args[]) {
-	try{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String input;
- 
-		while((input=br.readLine())!=null){
-			System.out.println(input);
+import java.io.*;
+public class args{
+	public static void main (String args[]) {
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String input;
+	 
+			while((input=br.readLine())!=null){
+				System.out.println(input);
 		}
  
-	}catch(IOException io){
-		io.printStackTrace();
-	}
-}	
+		}catch(IOException io){
+			io.printStackTrace();
+		}
+	}	
+}
